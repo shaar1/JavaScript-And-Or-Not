51d00765135cbe54bbebf3bea4787c12a14fbf78
Abdallah <abdallahchaar90@gmail.comh>
2021-01-07 09:18:10 +0100

    träning om operators

---
+++ main.js
@@ -0,0 +1,60 @@
+/* global console, alert*/
+
+/*
+
+( ! ) Not Egual
+
+( != ) ! Not, = Egual => (Not Egual)
+
+( !== ) Not Identical
+
+*/
+
+/*
+var Abdallah = "32";
+
+if (Abdallah != 50) {
+
+    alert("Good");
+
+}else{
+
+    alert("Bad");
+}
+*/
+/*
+var age = "32";
+
+if (age !== 50) {
+
+    alert("Good");
+
+}else{
+
+    alert("Bad");
+}
+*/
+/*
+var age = 32;
+
+if (age !== 50) {
+
+    alert("Good");
+
+}else{
+
+    alert("Bad");
+}
+*/
+
+var age = "32"; // Data Type String
+
+if (age !== 32) { // Data Type Number
+
+    alert("Good");
+
+}else{
+
+    alert("Bad");
+}
+
