68be1344e050a2015d06c4808f826bdf91a2acd3
Abdallah <abdallahchaar90@gmail.comh>
2021-01-07 10:11:12 +0100

    träning om or

--- main.js
+++ main.js
@@ -10,6 +10,8 @@
 
 ( && ) And
 
+( || ) or  allt + 7 = ||
+
 */
 
 /*
@@ -147,15 +149,119 @@ if (age == 32 && name == "Abdallah") {
 
 }
 */
-
+/*
 var
     name = "Abdallah",
 
-    age = "33",
+    age = "32",
 
     address = "KSA";
 
 if (age == 32 && name == "Abdallah" && address == "KSA") {
+
+    alert("Good");
+
+} else {
+
+    alert("Bad");
+
+}
+*/
+/*
+var
+    name = "Abdallah",
+
+    age = "32", // Data Type string
+
+    address = "KSA";
+
+if (age === 32 && // If The Age Value And Data Type = 32 Then True
+
+    name == "Abdallah" &&
+
+    address == "KSA") {
+
+    alert("Good");
+
+} else {
+
+    alert("Bad");
+}
+*/
+/*
+var
+    name = "Abdallah",
+
+    age = "32", // Data Type string
+
+    address = "KSA";
+
+if (age === "32" && // If The Age Value And Data Type = "32" Then True
+
+    name == "Abdallah" && // If The Name And Data Type = Abdallah Then True
+
+    address == "KSA") { // If The Addrees Value And Data Type = Ksa Then True
+
+    alert("Good Your Name Is " + name);
+
+} else {
+
+    alert("Bad");
+
+}
+*/
+/*
+var
+    name = "Abdallah",
+
+    age = 33, 
+
+    address = "KSA";
+
+    
+
+if (age === 32 || name == "Abdallah" || address == "KSA") { 
+
+    alert("Good");
+
+} else {
+
+    alert("Bad");
+
+}
+*/
+
+/*
+var
+    name = "Abdallah",
+
+    age = 33, 
+
+    address = "Egypt";
+
+    
+
+if (age === 32 || name === "Abdallah" || address === "KSA") { 
+
+    alert("Good");
+
+} else {
+
+    alert("Bad");
+
+}
+*/
+
+var
+    name = "Abdallah",
+
+    age = 33, 
+
+    address = "Egypt";
+
+    
+
+if (age === 32 || name === "Hassan" || address === "Egypt") { 
 
     alert("Good");
 
